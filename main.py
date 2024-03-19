b89f61db2c16985346a3f30f8c0078e9349875b5
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-03-19 15:12:15 +1100

    completed project

--- main.py
+++ main.py
@@ -8,7 +8,22 @@ answer = input(
 if answer == "left":
     awnser2 = input(
         "You come to a river, do you swim across or walk around? (walk/swim) ").lower()
+    if awnser2 == "walk":
+        print("you walked a while and ran out of food. You starved to death.")
+    elif awnser2 == "swim":
+        print("You swam across and got eaten by a hungry fish.")
+    else:
+        print("Invalid response. You Lose")
+
 elif answer == "right":
-    print("")
+    awnser2 = input(
+        "You come to a bridge, do you cross it or turn around? (cross/turn) ").lower()
+    if awnser2 == "cross":
+        print("You crossed the bridge and found a treasure chest full of gold.")
+    elif awnser2 == "turn":
+        print("You turned around and got hit by a car.")
+    else:
+        print("Invalid response. You Lose")
+
 else:
     print("That is not a valid direction. You lose")
